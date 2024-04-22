26ec0bf77181e360ac5f6cd7ccfd68d41692e911
knguro <167727224+knguro@users.noreply.github.com>
2024-04-21 20:13:01 -0600

    uplouding my first code in c.
    this is the first code that deals with a hello world in c.

---
+++ holsmundo.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    printf("hola mundo soy francisco\n"); 
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    printf("hola mundo soy francisco\n");
+    return 0;
+}
